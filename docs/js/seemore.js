c7854ae37d31f05bee6804afc6d6424a08ca722a
Vessiot Manon <manon.vessiot@hotmail.com>
2024-01-30 08:51:37 +0100

    translate hiking

--- docs/js/seemore.js
+++ docs/js/seemore.js
@@ -1,12 +1,17 @@
 function ShowDetails(id) {
   var content = document.getElementById(id + "-content");
-  var btn = document.getElementById(id + "-btn");
-    if (content.style.display == "none"){
-      content.style.display = "block";
-      btn.innerHTML  = "<b>< SEE LESS</b>";
-    }
-    else{
-      content.style.display = "none";
-      btn.innerHTML  = "<b>SEE MORE ></b>";
-    }
+  var btn_more = document.getElementById(id + "-btn-more");
+  var btn_less = document.getElementById(id + "-btn-less");
+  content.style.display = "block";
+  btn_less.style.display = "block";
+  btn_more.style.display = "none";
   }
+
+  function HideDetails(id) {
+    var content = document.getElementById(id + "-content");
+    var btn_more = document.getElementById(id + "-btn-more");
+    var btn_less = document.getElementById(id + "-btn-less");
+    content.style.display = "none";
+    btn_more.style.display = "block";
+    btn_less.style.display = "none";
+    }
